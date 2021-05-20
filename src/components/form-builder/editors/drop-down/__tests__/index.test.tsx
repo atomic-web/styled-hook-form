72c72774ba356bda74852d497fa0fa3624c69b0d
b.esmaili <beehnam.esmaili@gmail.com>
2021-05-20 17:02:43 +0430

    add dropdown test

---
+++ src/components/form-builder/editors/drop-down/__tests__/index.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import { FormBuilder } from "../../../../form-builder/form-builder";
+import { FormFieldType } from "../../../../form-builder/types";
+import 'jest-styled-components'
+
+describe("DropDown Editor", () => {
+  it("renders default value", () => {
+    const DropDownField = () => {
+      return (
+        <FormBuilder
+          fields={[
+            {
+              type: FormFieldType.DropDown,
+              options: [
+                { text: "opt1", value: "Opt1" },
+                { text: "opt2", value: "Opt2" },
+              ],
+              itemLabelKey: "text",
+              itemValueKey: "value",
+              defaultValue: "opt1",
+              name: "test",
+              label: "test",
+            },
+          ]}
+        />
+      );
+    };
+
+    let { container, getByText } = render(<DropDownField />);
+    expect(container.firstElementChild).toMatchSnapshot();
+  });
+});
+
+export {};
