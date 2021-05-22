edf34471726f440bce359bcc3174d730e4eac0cb
BEHNAM <beehnam.esmaili@gmail.com>
2021-05-22 17:29:26 +0430

    story: add infinite-scroll story

---
+++ src/components/form-builder/editors/drop-down/stories/infinite-scroll.tsx
@@ -0,0 +1,72 @@
+import { FormBuilder } from "components/form-builder/form-builder";
+import { FormField, FormFieldType } from "components/form-builder/types";
+import { Box, Button, Text } from "grommet";
+import { commerce } from "faker";
+
+let nextId = () => {
+  let id = 0;
+  return () => ++id;
+};
+
+const ID = nextId();
+
+export const InfiniteScroll = () => {
+  let fields: FormField[] = [
+    {
+      name: "productId",
+      label: "Product",
+      type: FormFieldType.DropDown,
+      itemLabelKey: "name",
+      itemValueKey: "id",
+      defaultValue: 0,
+      options: {
+        url: "/api/categories",
+        searchKey: "queryText",
+        extraParams: { blaa: "Blaa" },
+        mockResponse: (mock) => {
+          mock.onGet("/api/categories").reply(({ params }) => {
+            let data = new Array(20)
+              .fill(0)
+              .map(() => ({ id: ID(), name: commerce.productName() }));
+           
+              if (params.queryText) {
+              data = data.filter(
+                (e) =>
+                  e.name
+                    .toLowerCase()
+                    .indexOf(params.queryText.toLowerCase()) >= 0
+              );
+            }
+
+            return [200, data];
+          });
+        },
+      },
+    },
+  ];
+
+  return (
+    <Box width="medium">
+      <FormBuilder
+        fields={fields}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        <Text
+          margin={{
+            vertical: "medium",
+          }}
+        >
+          * Start typing in search box to filter result , but bear in mind that
+          as data is fake the search result may differ on each search.
+        </Text>
+        <Button label="Submit" type="submit" primary />
+      </FormBuilder>
+    </Box>
+  );
+};
+
+export default {
+  title: "Form Builder/Editors/DropDown/Infinite Scroll",
+};
