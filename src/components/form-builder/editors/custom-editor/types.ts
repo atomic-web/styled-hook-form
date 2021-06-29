711b1425e726bea33158ba9bf3c17d6cd9837532
b.esmaili <beehnam.esmaili@gmail.com>
2021-06-29 15:49:05 +0430

    fix CustomEditor prop type

--- src/components/form-builder/editors/custom-editor/types.ts
+++ src/components/form-builder/editors/custom-editor/types.ts
@@ -1,6 +1,6 @@
-import { FormEditorPropsBase, FormField } from "../../../form-builder/types";
+import { FormEditorPropsBase } from "../../../form-builder/types";
 import React from "react";
 
-export type CustomEditorProps = FormEditorPropsBase &{
-     editorComponent : React.ReactElement<FormField<any>>
-}
+export type CustomEditorProps = FormEditorPropsBase & {
+  editorComponent: React.ReactElement;
+};
