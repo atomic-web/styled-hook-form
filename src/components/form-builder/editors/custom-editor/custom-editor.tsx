8b09e62ba886e2f4f65a04aca4572e7692723356
b.esmaili <beehnam.esmaili@gmail.com>
2021-07-03 17:28:22 +0430

    custom editor watch props

--- src/components/form-builder/editors/custom-editor/custom-editor.tsx
+++ src/components/form-builder/editors/custom-editor/custom-editor.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { FormField } from "../../../form-builder/types";
 import { useSHFContext } from "../../../../context";
 import { Controller } from "react-hook-form";
 import { CustomEditorProps } from "./types";
 
 const CustomEditor: React.FC<FormField<CustomEditorProps>> = (props) => {
-  
-    let vrules = props.validationRules || {};
-    const {translate : T } = useSHFContext();
+  let vrules = props.validationRules || {};
+  const { translate: T } = useSHFContext();
 
-    let {
-      name,
-      defaultValue: initialValue,
-      methods,
-      required,
-      editorComponent,
-      label,
-      props : componentProps
-    } = props;
+  let {
+    name,
+    defaultValue: initialValue,
+    methods,
+    required,
+    editorComponent,
+    label,
+    props: componentProps,
+  } = props;
 
-    let control = methods?.control;
+  let control = methods?.control;
 
-    if (required) {
-        vrules.required = {
-          value: required,
-          message: T("required-msg", { name: label }),
-        };
+  if (required) {
+    vrules.required = {
+      value: required,
+      message: T("required-msg", { name: label }),
+    };
+  }
+
+  let [customProps, setCustomProps] = useState({});
+
+  useEffect(() => {
+    if (componentProps) {
+      setCustomProps(componentProps);
     }
+  }, [componentProps]);
 
-    return (
-        <Controller
-          name={name}
-          defaultValue={initialValue}
-          rules={vrules  as any}
-          control={control}
-          render={({ field }) => {
-            let editorElement = React.cloneElement(editorComponent,{
-                ...componentProps,
-                value:field.value,
-                onChange :(e :any)=> field.onChange(e),
-                methods
-            });
-            return editorElement;
-          }}
-        />
-    );
+  return (
+    <Controller
+      name={name}
+      defaultValue={initialValue}
+      rules={vrules as any}
+      control={control}
+      render={({ field }) => {
+        let editorElement = React.cloneElement(editorComponent, {
+          ...customProps,
+          value: field.value,
+          onChange: (e: any) => field.onChange(e),
+          methods,
+        });
+        return editorElement;
+      }}
+    />
+  );
 };
 
-export {CustomEditor}
+export { CustomEditor };
