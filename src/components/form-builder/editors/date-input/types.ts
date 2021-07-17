86210121f971845b18ad27f5a5f796ef8b24afa2
b.esmaili <beehnam.esmaili@gmail.com>
2021-07-17 13:43:04 +0430

    accept Date as default value

--- src/components/form-builder/editors/date-input/types.ts
+++ src/components/form-builder/editors/date-input/types.ts
@@ -5,5 +5,5 @@ export type DateInputProps = FormEditorPropsBase & {
     minDate?: string,
     maxDate?: string,
     dateInputProps?:GrommetDateInputProps
-    defaultValue: string | Date
+    defaultValue?: string | Date
 }
