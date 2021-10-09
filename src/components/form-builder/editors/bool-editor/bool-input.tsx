b55c15025b579380c52276f15e8759344fa3412d
b.esmaili <beehnam.esmaili@gmail.com>
2021-10-09 11:53:50 +0330

    add required to BoolEditor

--- src/components/form-builder/editors/bool-editor/bool-input.tsx
+++ src/components/form-builder/editors/bool-editor/bool-input.tsx
@@ -3,19 +3,29 @@ import { Controller } from "react-hook-form";
 import { BoolEditorProps } from "./types";
 import { CheckBox } from "grommet";
 import { FormField } from "../../types";
+import { useSHFContext } from "../../../../context";
 
 const BoolEditor = forwardRef<HTMLInputElement, FormField<BoolEditorProps>>(
   (props, ref) => {
     let vrules = props.validationRules || {};
+    const {translate : T } = useSHFContext();
 
     let {
       name,
       defaultValue: initialValue,
       shouldUnregister,
       controlType,
+      required,
       methods,
       label,
     } = props;
+
+    if (required) {
+      vrules.required = {
+        value: required,
+        message: T("required-msg", { name: label }),
+      };
+    }
 
     let control = methods?.control;
 
