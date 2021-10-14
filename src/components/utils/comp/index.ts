d233e2bb51eab9efc527608d747e4823925b32d2
Behnam Esmaili <beehnam.esmaili@gmail.com>
2021-10-14 16:13:56 +0330

    bug fix in isEmptyObject func

--- src/components/utils/comp/index.ts
+++ src/components/utils/comp/index.ts
@@ -1,8 +1,10 @@
+export const isEmptyObject = (obj: any) : boolean => {
+  if (!obj) return true;
 
-export const isEmptyObject = (obj : any)=>{
-    if (!obj)
-    return true;
+  if (!["object"].includes(typeof obj)) return false;
 
-    return Object.keys(obj).length === 0 ||
-           !Object.values(obj).some(p=>p!==undefined);
-}
+  return (
+    Object.keys(obj).length === 0 ||
+    !Object.values(obj).some((p) => !(p === undefined || isEmptyObject(p)))
+  );
+};
