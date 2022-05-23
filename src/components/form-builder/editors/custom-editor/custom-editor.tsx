dfa9b1a1c657ca86ca29d82a7d9f720e662c6674
behnam <beehnam.esmaili@gmail.com>
2022-05-23 23:44:27 +0430

    update version 1.0.0-rc.50

--- src/components/form-builder/editors/custom-editor/custom-editor.tsx
+++ src/components/form-builder/editors/custom-editor/custom-editor.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FormField } from "../../../form-builder/types";
 import { useFormBuilderContext } from "../../../../context";
 import { Controller } from "react-hook-form";
